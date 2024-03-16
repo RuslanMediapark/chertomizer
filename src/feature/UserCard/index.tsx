3514fd6f015f9eba71645fd42d8178367cdc30b9
Ruslan Matusevich <ruslan.matusevich@mediapark.com>
2024-03-16 21:13:15 +0100

    fix for deploy

--- src/feature/UserCard/index.tsx
+++ src/feature/UserCard/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useRef, useState } from "react";
+import { FC, useEffect, useRef } from "react";
 import NewPlayer from '@/assets/new.webp';
 import NormalUser from '@/assets/user.webp';
 
